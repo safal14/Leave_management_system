08fc4674c9bf1fe2abe5277ce17811a05e0dd100
Safal Acharya <acharyasafal51@gmail.com>
2026-01-02 17:54:23 +0545

    Create: created and migrated leave_type  model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_01_02_120735) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_02_120855) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -35,6 +35,13 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_02_120735) do
     t.integer "Total_Days"
     t.integer "Used_Days"
     t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "leave_types", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "description"
+    t.string "name"
     t.datetime "updated_at", null: false
   end
 end
