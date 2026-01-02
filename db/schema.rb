2f5aa35290e24e14551c9e8aa2d66d9ac3261422
Safal Acharya <acharyasafal51@gmail.com>
2026-01-02 17:51:25 +0545

    Create: created and migrated department model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_01_02_115356) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_02_120535) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "departments", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "description"
+    t.string "name"
+    t.datetime "updated_at", null: false
+  end
 
   create_table "employees", force: :cascade do |t|
     t.datetime "created_at", null: false
